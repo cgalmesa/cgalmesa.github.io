c206846ddf3e3da3dc46c1a1e1af66d2f43ae40f
cgalmesa <carmengalmesa@gmail.com>
2026-07-14 16:13:35 -0700

    Update global.ts

--- src/content/global.ts
+++ src/content/global.ts
@@ -35,30 +35,5 @@ export const globalContent = {
     { label: "Artwork", page: "Artwork", href: "/artwork" },
     { label: "Contact & CV", page: "Contact", href: "/contact" },
   ] satisfies NavigationItem[],
-  decorativeGifs: [
-    {
-      className: "alien alien-left alien-left-top",
-      src: "/images/decorations/spinning-alien.gif",
-      width: 96,
-      height: 96,
-    },
-    {
-      className: "alien alien-left alien-left-bottom",
-      src: "/images/decorations/waving.gif",
-      width: 122,
-      height: 122,
-    },
-    {
-      className: "alien alien-right alien-right-top",
-      src: "/images/decorations/aliens-walking.gif",
-      width: 170,
-      height: 120,
-    },
-    {
-      className: "alien alien-right alien-right-bottom",
-      src: "/images/decorations/spinning-alien.gif",
-      width: 90,
-      height: 90,
-    },
-  ] satisfies DecorativeGif[],
+  decorativeGifs: [] satisfies DecorativeGif[],
 } as const;
